050d3cd75cbdb2a1594355fdd93af15423148867
deirdreemoran <deirdreemoran@gmail.com>
2018-10-01 18:25:21 -0700

    updated host

--- dbcon.js
+++ dbcon.js
@@ -1,11 +1,32 @@
 var mysql = require('mysql');
+var express = require('express');
 
-var pool = mysql.createPool({
+var connection = mysql.createConnection({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_morand',
-  password        : '6177',
-  database        : 'cs340_morand'
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'deirdre',
+  database        : 'practice'
 });
 
-module.exports.pool = pool;
+connection.connect(function(error){
+	if(!!error){
+		console.log('Error');
+	}
+	ekse{
+		console.log('Connected');
+	}
+});
+app.get('/', function(req, resp){
+	//about mysql
+	connection.query("SELECT * FROM practice", function(error, rows, fields)
+	if(!!error){
+		console.log("Error in the query");
+	}else{
+		//parse with your rows/fields
+		console.log("Successful");
+	}
+});
+})
+app.listen(3306);
+
