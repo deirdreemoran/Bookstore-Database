18756db9c0283092b9776b203d05593ae4a1494b
Deirdre Moran <deirdreemoran@gmail.com>
2018-10-06 10:24:04 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,32 +1,14 @@
 var mysql = require('mysql');
-var express = require('express');
 
-var connection = mysql.createConnection({
-  connectionLimit : 10,
-  host            : 'localhost',
-  user            : 'root',
-  password        : 'deirdre',
-  database        : 'practice'
+var pool = mysql.createPool({
+	connectionLimit: 10,
+	host: 'localhost',
+	user: 'root',
+	password: 'deee',
+	database: 'employeerecognitions',
+	insecureAuth: true
+
 });
 
-connection.connect(function(error){
-	if(!!error){
-		console.log('Error');
-	}
-	ekse{
-		console.log('Connected');
-	}
-});
-app.get('/', function(req, resp){
-	//about mysql
-	connection.query("SELECT * FROM practice", function(error, rows, fields)
-	if(!!error){
-		console.log("Error in the query");
-	}else{
-		//parse with your rows/fields
-		console.log("Successful");
-	}
-});
-})
-app.listen(3306);
 
+module.exports.pool = pool;
